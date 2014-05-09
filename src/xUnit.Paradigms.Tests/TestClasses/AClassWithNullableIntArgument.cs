9dff4ae3296ade0a7890d4922cb2b97db56ec7ea
algirdasl <al@frontit.dk>
2014-05-09 17:32:17 +0300

    Fixed issue, when nullable values couldn't be passed into constructor because of incorrect parameter types validation;

---
+++ src/xUnit.Paradigms.Tests/TestClasses/AClassWithNullableIntArgument.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace xUnit.Paradigms.Tests
+{
+    class AClassWithNullableIntArgument
+    {
+        private readonly int? _s;
+
+        public int? ArgumentValue
+        {
+            get { return _s; }
+        }
+
+        public AClassWithNullableIntArgument(int? s)
+        {
+            _s = s;
+        }
+
+        [Fact]
+        public void TestMethod()
+        {
+            Assert.Equal(_s, 1);
+        }
+    }
+}
